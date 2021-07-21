f41f9051091e1e60cf4c7afdac8d22afa3e85c83
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-21 20:49:40 +0530

    moved the components folder inside the project

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      
+
     </View>
   );
 }
