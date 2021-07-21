abeeb1dd9e7d3d85e415d5bf2850bfe66cf5f5ca
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-21 20:44:18 +0530

    paste the same code for the functional component inside the PreviewImage.js file

---
+++ .expo-shared/components/PreviewImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const CollectionImages = () => {
+    return(
+        <Text>
+            CollectionImages
+        </Text>
+    );
+}
+
+export default CollectionImages;
