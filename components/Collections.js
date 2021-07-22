dab8d18a7a843d4eb7f2647c29c66cafb0594ee7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-22 20:03:55 +0530

    import the View component

--- components/Collections.js
+++ components/Collections.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, StyleSheet, Pressable} from 'react-native';
+import {Text, StyleSheet, Pressable, View} from 'react-native';
 
 const Collections = ({navigation}) => {
     return(
