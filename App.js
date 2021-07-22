71c4caf795fce5ca0a965067d77e12cad47af5c5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-22 19:42:05 +0530

    add the paddingTop property for the container attribute

--- App.js
+++ App.js
@@ -30,6 +30,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: Constants.statusBarHeight,
     alignItems: 'center',
     justifyContent: 'center',
   },
