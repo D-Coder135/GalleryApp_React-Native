ed6b1bad4a221484fede16db6cb9ab12493318ef
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-22 20:37:18 +0530

    update the color for the Icon tag

--- components/Collections.js
+++ components/Collections.js
@@ -8,7 +8,7 @@ const Collections = ({navigation}) => {
             <View style = {styles.header}>
                 <Text>
                     Collections
-                    <Icon name = {'images'} size = {35} color = {'black'}/>
+                    <Icon name = {'images'} size = {35} color = {'white'}/>
                 </Text>
             </View>
 
