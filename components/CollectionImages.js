ffd80bfc63bab28131c34985219832ab7b4727ff
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-23 23:13:09 +0530

    import the Pressable component

--- components/CollectionImages.js
+++ components/CollectionImages.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, Pressable,} from 'react-native';
 
 const CollectionImages = () => {
     return(
