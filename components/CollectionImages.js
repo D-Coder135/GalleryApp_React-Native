a82d8afec1a4dd37f79c7ea6764f4990122a33b4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-23 23:13:42 +0530

    import the View tag

--- components/CollectionImages.js
+++ components/CollectionImages.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Text, StyleSheet, Pressable,} from 'react-native';
+import {Text, StyleSheet, Pressable, View,} from 'react-native';
 
 const CollectionImages = () => {
     return(
