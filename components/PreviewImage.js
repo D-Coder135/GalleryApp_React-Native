ba2fe4e500916a01b7910eb04159cd6da8aaca55
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 19:33:01 +0530

    import the Icon component from the react-native-vector-icons/FontAwesome library

--- components/PreviewImage.js
+++ components/PreviewImage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, StyleSheet, View, Pressable} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const PreviewImage = () => {
     return(
