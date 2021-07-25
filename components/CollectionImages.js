70898800a4113cbbd17ce0781cd523811fdc622c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 22:05:28 +0530

    add a return statement for the function

--- components/CollectionImages.js
+++ components/CollectionImages.js
@@ -17,7 +17,7 @@ const CollectionImages = ({navigation, route}) => {
 
     const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
         const paddingToBottom = 20;
-        
+        return layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
     }
 
     return(
