9721e8b8726b4ad9c4c54f7947157e2321d03aa8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 19:32:30 +0530

    import the Pressable tag

--- components/PreviewImage.js
+++ components/PreviewImage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, Pressable} from 'react-native';
 
 const PreviewImage = () => {
     return(
